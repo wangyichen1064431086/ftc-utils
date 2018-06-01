2d85e728b4976097902a11e9341664a8ef00cec8
wangyichen1064431086 <1064431086@qq.com>
2018-06-01 16:32:02 +0800

    Modify the getCookie func

--- src/cookie.js
+++ src/cookie.js
@@ -1,11 +1,21 @@
-function GetCookie(name){
-  var start = document.cookie.indexOf(name+'='),
-      len = start+name.length+1,
-      end = document.cookie.indexOf(';',len);
-  if ((!start) && (name !== document.cookie.substring(0,name.length))) {return null;}
-  if (start === -1) {return null;}
-  if (end === -1) {end = document.cookie.length; }
-  return decodeURIComponent(document.cookie.substring(len,end));
+function getCookie(name) {
+  const cookieStr = document.cookie;
+  const nameLen = name.length;
+  const nameStartIndex = cookieStr.indexOf(name+'=');
+  if (nameStartIndex < 0) {
+    return null;
+  }
+  const valueStartIndex = nameStartIndex + nameLen + 1;
+  let valueEndIndex = cookieStr.indexOf(';', valueStartIndex);
+
+  if( !valueStartIndex && name !== cookieStr.substring(0, nameLen)) {////当startIndex为0的时候说明该name是第一个cookie,那name必须就是cookieStr.substring(0, name.length)
+    return null;
+  }
+  
+  if (valueEndIndex === -1) { //说明它就是最后一个cookie，后面没有;
+    valueEndIndex = cookieStr.length;
+  }
+  return decodeURIComponent(cookieStr.substring(valueStartIndex, valueEndIndex));
 }
 
 function SetCookie (name, value , sec , path , domain) {  
